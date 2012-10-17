69cc353f9870e7b142d75dd1df481e082d195c76
David Sivocha <david@sivocha.com>
2012-10-17 10:05:19 +0100

    First pass, Linear Interpolation
    Added code to allow UIView to take a single input and draw a line with
    it.

---
+++ FreehandDrawingTut/FreehandDrawingTut/LinearInterpView.h
@@ -0,0 +1,13 @@
+//
+//  LinearInterpView.h
+//  FreehandDrawingTut
+//
+//  Created by David Sivocha on 17/10/2012.
+//  Copyright (c) 2012 ThreeHive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LinearInterpView : UIView
+
+@end
---
+++ FreehandDrawingTut/FreehandDrawingTut/thcmLinearInterpView.h
@@ -0,0 +1,13 @@
+//
+//  thcmLinearInterpView.h
+//  FreehandDrawingTut
+//
+//  Created by David Sivocha on 17/10/2012.
+//  Copyright (c) 2012 ThreeHive. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface thcmLinearInterpView : NSView
+
+@end
